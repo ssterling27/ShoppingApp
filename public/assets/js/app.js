868e75d5efa9f96ee2f1ddd46551a34fd8370724
jypkr <wodusqkr1210@gmail.com>
2021-09-30 13:10:10 -0700

    logout added

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,22 +3,22 @@ axios.get('/api/items', {
     Authorization: `Bearer ${localStorage.getItem('token')}`
   }
 })
-.then(({data: items}) => {
-  items.forEach(item => {
-    const itemElem = document.createElement('div')
-    itemElem.classList = 'col-sm-3'
-    itemElem.style = 'margin-top: 25px;'
-    itemElem.innerHTML = `
+  .then(({ data: items }) => {
+    items.forEach(item => {
+      const itemElem = document.createElement('div')
+      itemElem.classList = 'col-sm-3'
+      itemElem.style = 'margin-top: 25px;'
+      itemElem.innerHTML = `
     <div class="card">
       <div class="card-body" id=${item._id}>
         <h5 class="card-title" style="margin-bottom: 20px;">${item.text}</h5>
         <p><a type="button" class="btn btn-info" href="${item.shopping_link}">Shopping Link</a></p>
         <p><a type="button" class="btn btn-danger" href="${item.video_link}">Video Link</a><p>
     `
-    document.getElementById('appearHere').append(itemElem)
+      document.getElementById('appearHere').append(itemElem)
+    })
   })
-})
-.catch(err => window.location = '/login.html')
+  .catch(err => window.location = '/login.html')
 
 document.getElementById('add').addEventListener('click', event => {
   event.preventDefault()
@@ -34,21 +34,26 @@ document.getElementById('add').addEventListener('click', event => {
       Authorization: `Bearer ${localStorage.getItem('token')}`
     }
   })
-  .then(({data: item}) => {
-    const itemElem = document.createElement('div')
-    itemElem.classList = 'col-sm-3'
-    itemElem.style = 'margin-top: 25px;'
-    itemElem.innerHTML = `
+    .then(({ data: item }) => {
+      const itemElem = document.createElement('div')
+      itemElem.classList = 'col-sm-3'
+      itemElem.style = 'margin-top: 25px;'
+      itemElem.innerHTML = `
     <div class="card">
       <div class="card-body" id=${item._id}>
         <h5 class="card-title" style="margin-bottom: 20px;">${item.text}</h5>
         <p><a type="button" class="btn btn-info" href="${item.shopping_link}">Shopping Link</a></p>
         <p><a type="button" class="btn btn-danger" href="${item.video_link}">Video Link</a><p>
     `
-    document.getElementById('appearHere').append(itemElem)
-    document.getElementById('product').value = ''
-    document.getElementById('shoppingLink').value = ''
-    document.getElementById('videoLink').value = ''
-  })
-  .catch(err => console.log(err))
+      document.getElementById('appearHere').append(itemElem)
+      document.getElementById('product').value = ''
+      document.getElementById('shoppingLink').value = ''
+      document.getElementById('videoLink').value = ''
+    })
+    .catch(err => console.log(err))
+})
+
+document.getElementById('logout').addEventListener('click', event => {
+  localStorage.removeItem('token')
+  window.location = './login.html'
 })
